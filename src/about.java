00bcf52af783250114edcd3ca570da3d1c9cc3a1
mgapas <31411833+mgapas@users.noreply.github.com>
2018-05-24 18:05:02 -0600

    Edited java source file

---
+++ src/about.java
@@ -0,0 +1,10 @@
+public class about {
+
+    public static void main (String[] args)
+    {
+        System.out.println("Name: Michael Gapas");
+        System.out.println("Favorite Language: C#");
+        System.out.println("I am studying software because I would like to get into game development.");
+    }
+
+}
